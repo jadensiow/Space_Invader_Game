994d726e289ce3bb19b9732681341b19ebab0d17
jianhong93 <jianhong93@gmail.com>
2021-03-09 10:46:08 +0800

    Space Invasion game completed

---
+++ js/constants.js
@@ -0,0 +1,62 @@
+const initialGameSummary = {
+  // Settings can be changed
+  // Players
+  playerBulletSpeed: 500,
+  moveSpeed: 300,
+  playerBulletCD: 0.5,
+  playerImage: "img/player1.png",
+  playerBulletImg: "img/bulletplayer1.png",
+
+  // Players special bullets
+  playerSpecCD: 2,
+  playerSpecBulletSpd: 200,
+  playerSpecCD2: 0,
+
+  // Enemy
+  enemyHorPad: 80, // horizontal padding to the left and right side
+  enemyPerRow: 8, // How many enemies per row
+  enemyVerPad: 100, // Top vertical padding
+  enemyVerSpace: 300, // between each row spacing of enemy
+  numEnemyrow: 2, //number of enemies per row
+  enemyHitsToDestroy: 1, // number of hits to destroy
+  maxEnemyCD: 10, // max cooldown of enemy shooting
+  minEnemyCD: 0.5, // min cooldown of enemy shooting
+  enemyBulletSpeed: 400, // Speed of enemy bullet
+  enemyImage: "img/enemy1.png",
+  enemyBulletImg: "img/bulletenemy1.png",
+
+  // Enemy speed
+  enemyHorSpd: 1000, // lower the faster of horizontal speed
+  enemyVerSpd: 1000, // lower the faster of vertical speed
+
+  enemyHorAngle: 50, // horizontal movement
+  enemyVerAngle: 10, // vertical movement
+
+  // Dont change
+  playerXPos: 0,
+  playerYPos: 0,
+  playerBullets: [],
+  playerCD: 0,
+  recordedTime: Date.now(),
+  enemy: [],
+  enemyBullets: [],
+
+  playerSpecBullet: [],
+  playerSpecBullet2: [],
+  playerSpecBulletImg: "img/bulletplayer2.png",
+
+  // Players special bullets second set
+
+  playerSpecBullet3: [],
+  playerSpecBullet4: [],
+  playerSpecBulletImg2: "img/bulletplayer4.png",
+
+  // PLayer Special bullets third set
+  playerSpecBullet5: [],
+  playerSpecBullet6: [],
+  playerSpecBulletImg3: "img/bulletplayer5.png",
+  playerSize: 0.07,
+  playerBulletSize: 0.04,
+  enemySize: 0.07,
+  enemyBulletSize: 0.05,
+};
---
+++ js/levels.js
@@ -0,0 +1,172 @@
+// these define the level properties to be overwritten
+
+const levelObjects = {
+  1: {
+    playerBulletSpeed: 500,
+    moveSpeed: 300,
+    playerBulletCD: 0.5,
+    playerImage: "img/player1.png",
+    playerBulletImg: "img/bulletplayer1.png",
+
+    // Players special bullets
+    playerSpecCD: 1,
+    playerSpecBulletSpd: 200,
+    playerSpecCD2: 0,
+
+    // Enemy
+    enemyHorPad: 80,
+    enemyPerRow: 8,
+    enemyVerPad: 100,
+    enemyVerSpace: 100,
+    numEnemyrow: 4,
+    enemyHitsToDestroy: 1,
+    maxEnemyCD: 12,
+    minEnemyCD: 0.5,
+    enemyBulletSpeed: 400,
+    enemyImage: "img/enemy1.png",
+    enemyBulletImg: "img/bulletenemy1.png",
+    // Enemy speed
+    enemyHorSpd: 1000, // lower the faster
+    enemyVerSpd: 1000, // lower the faster
+
+    enemyHorAngle: 50,
+    enemyVerAngle: 10,
+  },
+
+  2: {
+    playerBulletSpeed: 500,
+    moveSpeed: 600,
+    playerBulletCD: 0.2,
+    playerImage: "img/player2.png",
+    playerBulletImg: "img/bulletplayer2.png",
+
+    // Players special bullets
+    playerSpecCD: 1,
+    playerSpecBulletSpd: 200,
+    playerSpecCD2: 0,
+
+    // Enemy
+    enemyHorPad: 380,
+    enemyPerRow: 2,
+    enemyVerPad: 0,
+    enemyVerSpace: 0,
+    numEnemyrow: 1,
+    enemyHitsToDestroy: 30,
+    maxEnemyCD: 3,
+    minEnemyCD: 0.2,
+    enemyBulletSpeed: 700,
+    enemyImage: "img/enemy2.png",
+    enemyBulletImg: "img/bulletenemy2.png",
+    // Enemy speed
+    enemyHorSpd: 800, // lower the faster
+    enemyVerSpd: 1000, // lower the faster
+
+    enemyHorAngle: 80,
+    enemyVerAngle: 50,
+    playerSize: 0.07,
+    playerBulletSize: 0.07,
+    enemySize: 0.3,
+    enemyBulletSize: 0.1,
+  },
+  3: {
+    playerBulletSpeed: 600,
+    moveSpeed: 400,
+    playerBulletCD: 0.8,
+    playerImage: "img/player3.png",
+    playerBulletImg: "img/bulletplayer3.png",
+
+    // Players special bullets
+    playerSpecCD: 4,
+    playerSpecBulletSpd: 200,
+    playerSpecCD2: 0,
+
+    // Enemy
+    enemyHorPad: 80,
+    enemyPerRow: 5,
+    enemyVerPad: 100,
+    enemyVerSpace: 100,
+    numEnemyrow: 3,
+    enemyHitsToDestroy: 3,
+    maxEnemyCD: 4,
+    minEnemyCD: 0.5,
+    enemyBulletSpeed: 300,
+    enemyImage: "img/enemy3.png",
+    enemyBulletImg: "img/bulletenemy3.png",
+
+    // Enemy speed
+    enemyHorSpd: 500, // lower the faster
+    enemyVerSpd: 500, // lower the faster
+
+    enemyHorAngle: 200,
+    enemyVerAngle: 100,
+  },
+  4: {
+    playerBulletSpeed: 600,
+    moveSpeed: 600,
+    playerBulletCD: 0.5,
+    playerImage: "img/player4.png",
+    playerBulletImg: "img/bulletplayer4.png",
+
+    // Players special bullets
+    playerSpecCD: 2,
+    playerSpecBulletSpd: 200,
+    playerSpecCD2: 0,
+
+    // Enemy
+    enemyHorPad: 1000,
+    enemyPerRow: 4,
+    enemyVerPad: 100,
+    enemyVerSpace: 100,
+    numEnemyrow: 4,
+    enemyHitsToDestroy: 4,
+    maxEnemyCD: 2,
+    minEnemyCD: 0.5,
+    enemyBulletSpeed: 200,
+    enemyImage: "img/enemy4.png",
+    enemyBulletImg: "img/bulletenemy4.png",
+
+    // Enemy speed
+    enemyHorSpd: 1000, // lower the faster
+    enemyVerSpd: 1000, // lower the faster
+
+    enemyHorAngle: 100,
+    enemyVerAngle: 100,
+
+    playerSize: 0.07,
+    playerBulletSize: 0.04,
+    enemySize: 0.07,
+    enemyBulletSize: 0.03,
+  },
+
+  5: {
+    playerBulletSpeed: 500,
+    moveSpeed: 600,
+    playerBulletCD: 0.5,
+    playerImage: "img/player5.png",
+    playerBulletImg: "img/bulletplayer5.png",
+
+    // Players special bullets
+    playerSpecCD: 1,
+    playerSpecBulletSpd: 200,
+    playerSpecCD2: 0,
+
+    // Enemy
+    enemyHorPad: 100,
+    enemyPerRow: 10,
+    enemyVerPad: 100,
+    enemyVerSpace: 300,
+    numEnemyrow: 2,
+    enemyHitsToDestroy: 100,
+    maxEnemyCD: 10,
+    minEnemyCD: 0.1,
+    enemyBulletSpeed: 200,
+    enemyImage: "img/enemy5.png",
+    enemyBulletImg: "img/bulletenemy5.png",
+    // Enemy speed
+    enemyHorSpd: 1000, // lower the faster
+    enemyVerSpd: 1000, // lower the faster
+
+    enemyHorAngle: 50,
+    enemyVerAngle: 10,
+  },
+};
